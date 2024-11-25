7be8ab1732d00ff66692bde8e1bd476c4e77ebfc
AbsurdSyssie <ozzyd97@gmail.com>
2024-11-25 21:37:05 +0000

    Update firebase.js
    removed secret

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,16 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics"; // Remove this line if not using analytics
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD0ZOHo8PSWEHR--yS022jPyGwLNIVzpuY",
-  authDomain: "med-transcriber.firebaseapp.com",
-  projectId: "med-transcriber",
-  storageBucket: "med-transcriber.appspot.com",
-  messagingSenderId: "918225360557",
-  appId: "1:918225360557:web:56a8d8a59270a0ff019824",
-  measurementId: "G-9J0EXDVZ7B",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
